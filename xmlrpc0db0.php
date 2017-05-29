c5299bd831d4483eb802e014b258d36f0e5c98d7
Luca Santarella <luca.santarella@gmail.com>
2017-05-28 21:39:45 -0400

    Domain fix

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -2,13 +2,13 @@
   <service>
     <engineName>WordPress</engineName>
     <engineLink>https://wordpress.org/</engineLink>
-    <homePageLink>//marciano.websitejungledemo.com</homePageLink>
+    <homePageLink>//marcianoandtopazio.com</homePageLink>
     <apis>
-      <api name="WordPress" blogID="1" preferred="true" apiLink="//marciano.websitejungledemo.com/xmlrpc.php" />
-      <api name="Movable Type" blogID="1" preferred="false" apiLink="//marciano.websitejungledemo.com/xmlrpc.php" />
-      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="//marciano.websitejungledemo.com/xmlrpc.php" />
-      <api name="Blogger" blogID="1" preferred="false" apiLink="//marciano.websitejungledemo.com/xmlrpc.php" />
-      	<api name="WP-API" blogID="1" preferred="false" apiLink="//marciano.websitejungledemo.com/?rest_route=/" />
+      <api name="WordPress" blogID="1" preferred="true" apiLink="//marcianoandtopazio.com/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="//marcianoandtopazio.com/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="//marcianoandtopazio.com/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="//marcianoandtopazio.com/xmlrpc.php" />
+      	<api name="WP-API" blogID="1" preferred="false" apiLink="//marcianoandtopazio.com/?rest_route=/" />
 	    </apis>
   </service>
 </rsd>
